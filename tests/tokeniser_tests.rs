c589349d9e2798d8fa1d6c6aa6cc33bd38e3303c
EthanVIII <61584567+EthanVIII@users.noreply.github.com>
2024-06-16 22:25:01 +1000

    Add tests

--- tests/tokeniser_tests.rs
+++ tests/tokeniser_tests.rs
@@ -3,7 +3,7 @@
 mod tests {
     use std::panic::catch_unwind;
     use neyman::parse::*;
-    
+
     #[test]
     fn parse_keywords() {
         let x: Vec<char> = "\
@@ -20,26 +20,35 @@ mod tests {
                         Token::EOL]
         )
     }
-    
+
     #[test]
     fn invalid_chars() {
         let x: Vec<char> = "~".chars().collect();
         let result = catch_unwind( || {tokenise(x)});
         assert!(result.is_err());
     }
+
+    #[test]
+    fn invalid_string() {
+        let x: Vec<char> = "\"test\" \" ".chars().collect();
+        let result = catch_unwind( || {tokenise(x)});
+        assert!(result.is_err());
+    }
     
+
     #[test]
     fn parse_literals() {
-        let x: Vec<char> = "\"Hello World+\" 1027384 \n".chars().collect();
+        let x: Vec<char> = "\"Hello World+\" \"\" 1027384 \n".chars().collect();
         let result = tokenise(x);
         assert_eq!(result,
         vec![
             Token::Literal(String::from("Hello World+")),
+            Token::Literal(String::from("")),
             Token::Literal(String::from("1027384")),
             Token::EOL
         ]);
     }
-    
+
     #[test]
     fn parse_ids() {
         let x: Vec<char> = "id_1 m1 1m _m\n".chars().collect();
